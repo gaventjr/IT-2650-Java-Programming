8e92b77ebb9f4bd675cad70a2686784608489aeb
George Avent <gaventjr@gmail.com>
2018-06-13 23:39:30 -0400

    First Program
    First println

---
+++ First Program/src/first/program/FirstProgram.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first.program;
+
+/**
+ *
+ * @author Palad
+ */
+public class FirstProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello reader.") ;
+        System.out.println("Welcome to Java");
+        System.out.println("Let's demostrate a simple calclation.");
+        int answer;
+        answer = 2+2;
+        System.out.println("2 plus 2 is " + answer);
+    }
+    
+}
